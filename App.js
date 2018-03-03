495c8ffa8103c99ecffd0e1c2802b0badbe18cd7
ARoman <qpbd@ukr.net>
2018-03-03 15:36:10 +0200

    added new method to open web page

--- App.js
+++ App.js
@@ -1,22 +1,30 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import { WebBrowser } from 'expo';
+import {StyleSheet, Text, View} from 'react-native';
+import {WebBrowser} from 'expo';
 
 export default class App extends React.Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-      </View>
-    );
-  }
+
+    openWebPage = async (url) => {
+        if (!url) throw 'MISSED_PARAMS';
+
+        let result = await WebBrowser.openBrowserAsync(url);
+        this.setState({result});
+    };
+
+    render() {
+        return (
+            <View style={styles.container}>
+                <Text>Open up App.js to start working on your app!</Text>
+            </View>
+        );
+    }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
 });
