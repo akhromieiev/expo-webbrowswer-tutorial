090756ef55df374983ba12b908398c87c3215959
ARoman <qpbd@ukr.net>
2018-03-03 14:35:34 +0200

    added webbrowswer

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { WebBrowser } from 'expo';
 
 export default class App extends React.Component {
   render() {
